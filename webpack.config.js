8b0072ad023f23e3c1aee1f28b1bb3573cfb5fd9
Simon Bailey <simon@newtriks.com>
2015-05-12 10:08:23 +0100

    feat: webpack configuration
    Signed-off-by: Simon Bailey <simon@newtriks.com>

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var webpack = require('webpack');
+
+module.exports = {
+
+  // Entry point for the bundle, path and filename to main module
+  entry: './test/ArtistSpec',
+
+  output: {
+    // Output directory as an absolute path
+    path: './test',
+    // Filename as relative path within output path
+    filename: 'spec.js',
+    // Output path from the view of the JS/HTML
+    publicPath: '/test/'
+  },
+
+  module: {
+    // Loader required for JSX
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  }
+
+};
