b903a2f07886eaca7570d29bb77cc7a65ed18d78
ijones16 <ian@egghead.io>
2016-09-30 15:59:33 +0100

    correct formatting

--- test/ArtistSpec.js
+++ test/ArtistSpec.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactTestUtils from 'react-addons-test-utils';
 import Artist from '../src/Artist';
+
+import ReactTestUtils from 'react-addons-test-utils';
 
 describe('Artist', () => {
   var component, contents;
@@ -8,7 +9,7 @@ describe('Artist', () => {
   beforeEach(() => {
     // React Changed test utils in 0.14.x
     // https://facebook.github.io/react/blog/2015/10/07/react-v0.14.html
-    component = ReactTestUtils.renderIntoDocument(<Artist name="Run the Jewels" />);
+    component = ReactTestUtils.renderIntoDocument( <Artist name="Run the Jewels" /> );
     contents = ReactTestUtils.scryRenderedDOMComponentsWithClass(component, 'artist')
   });
 
